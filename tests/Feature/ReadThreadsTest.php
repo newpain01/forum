f40d01e187b83297a3250914d86439cf864c2b1d
Igor Taskovski <newpain01@gmail.com>
2017-03-17 22:37:16 +0800

    Posting reply in forum

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -17,7 +17,7 @@ class ReadThreadsTest extends TestCase
     /** @test */
     public function a_user_can_view_all_threads()
     {
-        $this->get('/threads')
+        $this->get($this->thread->path())
              ->assertSee($this->thread->title);
         // $response->assertStatus(200); 
     }
@@ -25,7 +25,7 @@ class ReadThreadsTest extends TestCase
     /** @test */
     public function a_user_can_read_a_single_thread()
     {
-        $this->get('/threads/' . $this->thread->id)
+        $this->get($this->thread->path())
              ->assertSee($this->thread->title);
     }
 
@@ -33,7 +33,7 @@ class ReadThreadsTest extends TestCase
     public function a_user_can_read_replies_that_are_associated_with_a_thread()
     {
         $reply = factory('App\Reply')->create(['thread_id' => $this->thread->id]);
-        $this->get('/threads/' . $this->thread->id)
+        $this->get($this->thread->path())
              ->assertSee($reply->body);
 
     }
