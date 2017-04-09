dcc133f9996e0a817efad8d18d34a931450aed20
Igor Taskovski <newpain01@gmail.com>
2017-04-10 01:40:31 +0800

    Add Validation for Threads and Replies

--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -12,23 +12,35 @@ class ParticipateInForumTest extends TestCase
 	/** @test */
     public function unauthenticated_users_may_not_add_replies()
     {
-    	$this->expectException('Illuminate\Auth\AuthenticationException');
     	
         $this->withExceptionHandling()
-             ->post('/threads/some-channel/1/replies', [])
-             ->assertRedirect('/login');
+        ->post('/threads/some-channel/1/replies', [])
+        ->assertRedirect('/login');
     }
+    
     /** @test */
     public function an_authenticated_user_may_participate_in_forum_threads()
     {
-        $this->be($user = factory('App\User')->create());
+        $this->signIn();
 
-        $thread = factory('App\Thread')->create();
-        $reply = factory('App\Reply')->make();
+        $thread = create('App\Thread');
+        $reply = make('App\Reply');
+
         $this->post($thread->path() . '/replies', $reply->toArray());
-        $this->assertTrue(true);
 
         $this->get($thread->path())
         	 ->assertSee($reply->body);
+    }
+
+    /** @test */
+    public function a_reply_requires_a_body()
+    {
+        $this->withExceptionHandling()->signIn();
+
+        $thread = create('App\Thread');
+        $reply = make('App\Reply', ['body' => null]);
+
+        $this->post($thread->path() . '/replies', $reply->toArray())
+            ->assertSessionHasErrors('body');
     }
 }
